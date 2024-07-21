343a2821a4949194bbf77dd62ea20bf1621eb7d2
KRITIKA BENJWAL <89837049+Kritika11052005@users.noreply.github.com>
2024-07-21 21:56:10 +0530

    Add files via upload

---
+++ LeetCode.java
@@ -0,0 +1,45 @@
+import java.util.*;
+public class LeetCode {//implementation of Linked List
+static class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data=data;
+        this.next=null;
+    }
+}
+static class stack{
+    public static Node head;
+    public static boolean isEmpty(){
+        return head==null;
+    }
+    //Push function
+    public static void push(int data){
+        Node newNode=new Node(data);
+        if(isEmpty()){
+            head=newNode;
+            return;
+        }
+        newNode.next=head;
+        head=newNode;
+    }
+    //pop function
+    public static int pop(){
+        if(isEmpty()){
+            return -1;
+        }
+        int top=head.data;
+        head=head.next;
+        return top;
+    }
+    //peek function
+    public static int peek(){
+        if(isEmpty()){
+            return -1;
+        }
+        return head.data;
+    }
+}
+        
+    }
+
